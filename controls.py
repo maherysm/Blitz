d8e82018952bac9a3e83c802ed08ec609b955ee5
maherysm <maherysm@hotmail.com>
2024-07-25 14:57:04 -0400

    optimize mouse controls

--- controls.py
+++ controls.py
@@ -1,29 +1,27 @@
 import pygame
 from constants import *
 
-class Controls():
-    #checks if mouse is in the area of a certain object
-    def mouseInArea(self, mouseCoords, topLeftCoord, objSize):
-        if mouseCoords[0] >= topLeftCoord[0] and \
-                mouseCoords[0] <= topLeftCoord[0] + objSize[0] and \
-                mouseCoords[1] >= topLeftCoord[1] and \
-                mouseCoords[1] <= topLeftCoord[1] + objSize[1]:
-            return True
-        return False
+class Controls:
+    @staticmethod
+    def mouseInArea(mouseCoords, topLeftCoord, objSize):
+        x_in_range = topLeftCoord[0] <= mouseCoords[0] <= topLeftCoord[0] + objSize[0]
+        y_in_range = topLeftCoord[1] <= mouseCoords[1] <= topLeftCoord[1] + objSize[1]
+        return x_in_range and y_in_range
 
-    #checks is mouse is hovering over a valid card (parameter is an array of coordinates rather than one coordinate)
     def hoveringOverCard(self, mouseCoords, playerCoords):
-        for i in range(len(playerCoords)):
-            if self.mouseInArea(mouseCoords, playerCoords[i], (CARD_SIZE_X, CARD_SIZE_Y)):
+        for i, coord in enumerate(playerCoords):
+            if self.mouseInArea(mouseCoords, coord, (CARD_SIZE_X, CARD_SIZE_Y)):
                 return i
         return -1
 
-    def leftButtonClick(self):
+    @staticmethod
+    def leftButtonClick():
         return pygame.mouse.get_pressed(3)[0]
 
-    def rightButtonClick(self):
+    @staticmethod
+    def rightButtonClick():
         return pygame.mouse.get_pressed(3)[2]
 
-    def getMousePos(self):
+    @staticmethod
+    def getMousePos():
         return pygame.mouse.get_pos()
-
